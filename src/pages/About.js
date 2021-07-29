629804ec5dce778553fe01e94251149a6bcaa233
Cotldus <78590013+cotldus@users.noreply.github.com>
2021-07-29 12:22:37 +0800

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutPage = () => (
+    <>
+    <h1>About Me</h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec tellus scelerisque, viverra dolor eu, auctor tellus. Aliquam porttitor pulvinar ligula et aliquam. Aenean magna ipsum, dignissim sed volutpat vitae, mattis vitae justo. Suspendisse vel mauris eget neque egestas porttitor id vel turpis. Nam laoreet vitae nulla pellentesque viverra. Nunc at felis at est malesuada maximus sed eget arcu. Vivamus placerat lorem ligula, quis aliquet arcu aliquam quis. Vivamus leo orci, iaculis in risus ut, aliquet efficitur dolor. Ut euismod metus orci, sit amet vestibulum orci ultricies vitae. Aliquam et iaculis augue. Nulla purus risus, iaculis id odio mollis, faucibus tincidunt est. Morbi dictum nulla non convallis ultricies. Nulla aliquet lobortis quam, vel tempus mi consectetur aliquam. Morbi vitae mattis justo, eget faucibus felis. Suspendisse potenti. Morbi non hendrerit nisi.</p>
+    
+    <p>Maecenas sit amet gravida nisi. Phasellus at arcu nec urna commodo faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut velit vel nunc viverra sodales sit amet eu leo. Suspendisse ac metus aliquet, dapibus purus vel, lobortis justo. Duis nec pharetra velit. Proin pretium massa placerat malesuada sollicitudin. Pellentesque scelerisque suscipit massa, at porttitor ex. Donec elementum est cursus eleifend iaculis. Nunc consequat, purus nec aliquam dignissim, nulla augue auctor nisl, sit amet ornare urna erat eget nulla. Mauris posuere consectetur tellus, ut efficitur elit porta vitae. Etiam hendrerit molestie viverra. Vestibulum et felis enim. Proin tincidunt placerat interdum. Proin sit amet nisl eros.</p>
+    </>
+)
+
+export default AboutPage
